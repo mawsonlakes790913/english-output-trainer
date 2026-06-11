30170aff2c806f3e97a79fe1759df9f08da50f35
mawsonlakes790913 <liuniao790913@gmail.com>
2026-06-11 15:40:55 +0900

    feat: initialize Spring Boot project

---
+++ english-output-trainer/src/main/java/com/example/demo/EnglishOutputTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishOutputTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishOutputTrainerApplication.class, args);
+	}
+
+}
