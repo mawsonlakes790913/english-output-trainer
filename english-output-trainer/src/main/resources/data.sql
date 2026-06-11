d6ba6e449b3ec7fcf1a70cadbb25517e1da58ba7
mawsonlakes790913 <liuniao790913@gmail.com>
2026-06-11 18:02:12 +0900

    add initial data

---
+++ english-output-trainer/src/main/resources/data.sql
@@ -0,0 +1,84 @@
+INSERT INTO questions
+(
+    japanese_text,
+    english_text,
+    alternative_answer,
+    condition,
+    difficulty
+)
+VALUES
+(
+    '私は英語を勉強するために毎日練習しています。',
+    'I practice every day to study English.',
+    'I practice every day in order to study English.',
+    'to不定詞',
+    '初級'
+);
+
+INSERT INTO questions
+(
+    japanese_text,
+    english_text,
+    alternative_answer,
+    condition,
+    difficulty
+)
+VALUES
+(
+    '彼はその仕事を終えるために残業した。',
+    'He worked overtime to finish the task.',
+    NULL,
+    'to不定詞',
+    '初級'
+);
+
+INSERT INTO questions
+(
+    japanese_text,
+    english_text,
+    alternative_answer,
+    condition,
+    difficulty
+)
+VALUES
+(
+    '彼女は英語を話すことが好きです。',
+    'She likes speaking English.',
+    'She likes to speak English.',
+    '動名詞',
+    '初級'
+);
+
+INSERT INTO questions
+(
+    japanese_text,
+    english_text,
+    alternative_answer,
+    condition,
+    difficulty
+)
+VALUES
+(
+    '彼は宿題を終えてからテレビを見た。',
+    'He watched TV after finishing his homework.',
+    NULL,
+    '分詞構文',
+    '中級'
+);
+
+INSERT INTO questions
+(
+    japanese_text,
+    english_text,
+    alternative_answer,
+    condition,
+    difficulty
+)
+VALUES
+(
+    'その知らせは私を驚かせた。',
+    'The news surprised me.',
+    NULL,
+    '無生物主語',
+    '中級'
+);
