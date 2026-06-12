5c208a297188b39e72b437d36b1016dd2d313593
mawsonlakes790913 <liuniao790913@gmail.com>
2026-06-12 23:52:04 +0900

    Implement extracting question randomly

--- english-output-trainer/src/main/java/com/example/demo/controller/StudyController.java
+++ english-output-trainer/src/main/java/com/example/demo/controller/StudyController.java
@@ -1,40 +1,76 @@
 package com.example.demo.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import java.util.List;
+
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.demo.entity.Question;
 import com.example.demo.service.StudyServiceImpl;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 
 
-
+@SessionAttributes("questions")
 @Controller
 @RequiredArgsConstructor
 public class StudyController {
 	
 	private final StudyServiceImpl studyService;
 	
-	@GetMapping("/study")
-	public String getStudy(Model model,
-	        @PageableDefault(page = 0, size = 1) Pageable pageable) {
+	// @GetMapping("/study")
+	// public String getStudy(Model model,
+	        // @PageableDefault(page = 0, size = 1) Pageable pageable) {
 
-	    Page<Question> questionPage = studyService.getQuestion(pageable);
+	    // Page<Question> questionPage = studyService.getQuestion(pageable);
 	    
 	    // Question question = questionPage.getContent().get(0);
 
-	    model.addAttribute(
-	    	    "question",
-	    	    questionPage.getContent());
-	    model.addAttribute("page", questionPage);
+	    // model.addAttribute(
+	    	    // "question",
+	    	    // questionPage.getContent());
+	    // model.addAttribute("page", questionPage);
 
-	    return "study";
+	    // return "study";
+	// }
+
+	@GetMapping("/study")
+	public String getStudy(Model model,
+						   HttpSession session,
+						   @RequestParam(defaultValue = "0") int page){
+		// ① Sessionになければ取得して保存
+		if (session.getAttribute("questions") == null) {
+			List<Question> questions = studyService.getRandomQuestion();
+			session.setAttribute("questions", questions);
+		}
+		
+		// ② Sessionからquestions取得
+		List<Question> questions = (List<Question>) session.getAttribute("questions");
+		
+		// ③ page番目の問題を取り出す
+		Question question = questions.get(page);
+		
+		// ④ HTMLが必要な情報をModelへ格納
+		model.addAttribute("question", question);
+		model.addAttribute("currentPage", page + 1);
+		model.addAttribute("totalPages", questions.size());
+		model.addAttribute("hasPrevious", page > 0);
+		model.addAttribute(
+		        "hasNext",
+		        page < questions.size() - 1);
+
+		// ⑤ study.htmlを返す
+		return "study";
+		
 	}
+	
 	
 }
--- english-output-trainer/src/main/java/com/example/demo/service/StudyServiceImpl.java
+++ english-output-trainer/src/main/java/com/example/demo/service/StudyServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Question;
@@ -18,9 +20,10 @@ public class StudyServiceImpl implements StudyService {
 	
 	private final QuestionRepository repository;
 	
-	public Page<Question> getQuestion(Pageable pageable){
+	// public Page<Question> getQuestion(Pageable pageable){
 		// 問題一覧取得
-		return repository.findAll(pageable);
+		// return repository.findAll(pageable);
+		
 		// Page<Question> questionList = repository.findAll(pageable);
 		// MyBatisなら
 		// ①まずListへ格納
@@ -29,5 +32,16 @@ public class StudyServiceImpl implements StudyService {
 		// long count = repository.count();
 		// ③Pageのインスタンス生成
 		// return new PageImpl<Question>(questionList, pageable, count);
+	// }
+
+	//public List<Question> getQuestion(){
+		//return repository.findAll();
+	//}
+	
+	public List<Question> getRandomQuestion(){
+		
+		List<Question> extractedQuestions = repository.findAll();
+		Collections.shuffle(extractedQuestions);
+		return extractedQuestions;
 	}
 }
