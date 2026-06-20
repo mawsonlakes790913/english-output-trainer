d839ce12e625f69571c9c121ae2ef1117aab6a52
mawsonlakes790913 <liuniao790913@gmail.com>
2026-06-21 02:16:13 +0900

    feat: implement standard validation for signup form

--- english-output-trainer/src/main/java/com/example/demo/form/SignupForm.java
+++ english-output-trainer/src/main/java/com/example/demo/form/SignupForm.java
@@ -1,11 +1,31 @@
 package com.example.demo.form;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SignupForm {
+	@NotBlank
+	@Length(min = 8, max = 20)
+	@Pattern( regexp = "^[a-zA-Z0-9]+$")
 	private String userId;
+	
+	@Min(0)
+	@Max(120)
 	private Integer age;
+	
+	@NotBlank
+	@Length(min = 12, max = 30)
+	@Pattern( regexp = "^[a-zA-Z0-9]+$")
 	private String password;
+	
+	@NotBlank
+	@Length(min = 12, max = 30)
+	@Pattern( regexp = "^[a-zA-Z0-9]+$")	
 	private String passwordConfirm;
 }
