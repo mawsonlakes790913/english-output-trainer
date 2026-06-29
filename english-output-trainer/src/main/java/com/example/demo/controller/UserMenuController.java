89e0c7c9916fd153fdcc49894d42faf9794610c9
mawsonlakes790913 <liuniao790913@gmail.com>
2026-06-30 04:38:09 +0900

    feat: add user profile page

--- english-output-trainer/src/main/java/com/example/demo/controller/UserMenuController.java
+++ english-output-trainer/src/main/java/com/example/demo/controller/UserMenuController.java
@@ -1,14 +1,37 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Users;
+import com.example.demo.service.UserServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 
 public class UserMenuController {
+	
+	private final UserServiceImpl userServiceImpl;
 	
 	@GetMapping("/menu")
 	public String getUserMenu() {
 		return "userMenu";
+	}
+	
+	@GetMapping("/user/profile")
+	public String getUserProfile(
+	        @AuthenticationPrincipal UserDetails loginUser,
+	        Model model) {
+
+	    Users user = userServiceImpl.getUserOne(loginUser.getUsername());
+
+	    model.addAttribute("user", user);
+
+	    return "user/profile";
 	}
 }
