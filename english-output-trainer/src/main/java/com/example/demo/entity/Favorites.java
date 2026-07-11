f327790083db44db63ff1ad4a78e827ead08a0cb
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-12 04:02:18 +0900

    feat: add createdAt field to Favorites entity

--- english-output-trainer/src/main/java/com/example/demo/entity/Favorites.java
+++ english-output-trainer/src/main/java/com/example/demo/entity/Favorites.java
@@ -1,11 +1,15 @@
 package com.example.demo.entity;
 
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.EmbeddedId;
 
 
 public class Favorites {
 	@EmbeddedId
 	private FavoritesKey favoritesKey;
+	
+	private LocalDateTime createdAt;
 	
 }
