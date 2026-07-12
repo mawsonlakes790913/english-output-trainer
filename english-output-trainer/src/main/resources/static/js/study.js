61a41d6952e49cf17c35e2f911612d2b7b8319e7
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-13 02:53:34 +0900

    feat: add click event to favorite button

--- english-output-trainer/src/main/resources/static/js/study.js
+++ english-output-trainer/src/main/resources/static/js/study.js
@@ -7,4 +7,18 @@ function showAnswer() {
     document.getElementById("evaluationArea").style.display = "flex";
 
     document.getElementById("answerButton").style.display = "none";
+    
+}
+
+
+const favoriteButton = document.getElementById("favoriteButton");
+
+if (favoriteButton) {
+    favoriteButton.addEventListener("click", function () {
+
+        const questionId = favoriteButton.dataset.questionId;
+
+        console.log(questionId);
+
+    });
 }
