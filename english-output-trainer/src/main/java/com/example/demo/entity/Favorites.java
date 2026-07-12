900fe05108401a9bd88e78b4186f67cf58b6b6c2
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-12 15:55:38 +0900

    fix: add @Table annotation to Favorites entity

--- english-output-trainer/src/main/java/com/example/demo/entity/Favorites.java
+++ english-output-trainer/src/main/java/com/example/demo/entity/Favorites.java
@@ -5,8 +5,12 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
+@Table(name = "favorites")
 public class Favorites {
 	@EmbeddedId
 	private FavoritesKey favoritesKey;
