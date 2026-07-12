000722f1de8355681efab85fa80a596850cd629a
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-12 16:04:07 +0900

    feat: add favorite registration and removal logic

---
+++ english-output-trainer/src/main/java/com/example/demo/service/FavoritesService.java
@@ -0,0 +1,55 @@
+package com.example.demo.service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.Favorites;
+import com.example.demo.entity.FavoritesKey;
+import com.example.demo.entity.Users;
+import com.example.demo.repository.FavoritesRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class FavoritesService {
+	
+	private final UserServiceImpl userServiceImpl;
+	private final FavoritesRepository favoritesRepository;
+	
+	public boolean toggleFavorite(String loginUser, long questionId) {
+		
+		// ユーザー情報を取得
+		Users user = userServiceImpl.getUserOne(loginUser);
+		
+		// 複合キー情報を取得
+		FavoritesKey key = new FavoritesKey();
+		key.setUserId(user.getId());
+		key.setQuestionId(questionId);
+		
+		// 存在確認とINSERT及びDELETE処理
+		Optional<Favorites> optionalFavorites =
+				favoritesRepository.findByFavoritesKey(key);
+		
+		if (optionalFavorites.isEmpty()) {
+			//ここでINSERT
+		    Favorites favorite = new Favorites();
+		    favorite.setFavoritesKey(key);
+		    favorite.setCreatedAt(LocalDateTime.now());
+
+		    favoritesRepository.save(favorite);
+		    return true;
+		    
+		} else {
+			//ここでDELETE
+			favoritesRepository.deleteById(key);
+			return false;
+
+		}
+	}
+	
+}
