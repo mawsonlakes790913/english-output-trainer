15c4cb781c1ffea38cf503d9978f362dae931ede
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-12 15:57:33 +0900

    feat: add findByFavoritesKey to FavoritesRepository

--- english-output-trainer/src/main/java/com/example/demo/repository/FavoritesRepository.java
+++ english-output-trainer/src/main/java/com/example/demo/repository/FavoritesRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Favorites;
 import com.example.demo.entity.FavoritesKey;
 
 public interface FavoritesRepository extends JpaRepository<Favorites, FavoritesKey> {
+	
+	Optional<Favorites> findByFavoritesKey(FavoritesKey favoritesKey);
+	
 }
