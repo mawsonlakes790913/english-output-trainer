b9caf59fd9f47a905e71557bed74c8e30752d8d3
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-13 17:09:47 +0900

    feat: add favorite list page controller

--- english-output-trainer/src/main/java/com/example/demo/controller/FavoritesController.java
+++ english-output-trainer/src/main/java/com/example/demo/controller/FavoritesController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.entity.Question;
+import com.example.demo.entity.Users;
 import com.example.demo.service.FavoritesService;
+import com.example.demo.service.UserServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class FavoritesController {
 	
 	private final FavoritesService favoritesService;
+	private final UserServiceImpl userServiceImpl;
 	
 	@PostMapping("/favorite/toggle")
 	@ResponseBody
@@ -26,5 +34,17 @@ public class FavoritesController {
 	    return favoritesService.toggleFavorite(
 	            loginUser.getUsername(),
 	            questionId);
+	}
+	
+	@GetMapping("/favorites/list")
+	public String getFavoritesList(@AuthenticationPrincipal UserDetails loginUser,
+								   Model model) {
+		Users user =
+		        userServiceImpl.getUserOne(loginUser.getUsername());
+
+		List<Question> favoritesList = favoritesService.getFavoritesList(user.getId());
+		
+		model.addAttribute("favoritesList", favoritesList);
+		return "favorite/list";
 	}
 }
