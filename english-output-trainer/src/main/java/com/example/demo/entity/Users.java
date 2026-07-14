2d3373fc8d9c541e39400a36c1832be08847dc31
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-14 16:08:30 +0900

    refactor: add Favorites relationship to Users

--- english-output-trainer/src/main/java/com/example/demo/entity/Users.java
+++ english-output-trainer/src/main/java/com/example/demo/entity/Users.java
@@ -1,9 +1,12 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -11,14 +14,17 @@ import lombok.Data;
 @Entity
 @Table(name = "users")
 public class Users {
-	
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String userId;
 
     private String password;
-    
+
     private String role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Favorites> favorites;
 }
