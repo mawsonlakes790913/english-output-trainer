06ac5e7df88a08e8a0e5beda07c35dc2d0ca63e7
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-14 16:07:57 +0900

    refactor: add Favorites relationship to Question

--- english-output-trainer/src/main/java/com/example/demo/entity/Question.java
+++ english-output-trainer/src/main/java/com/example/demo/entity/Question.java
@@ -1,10 +1,13 @@
 package com.example.demo.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,4 +37,7 @@ public class Question {
     @Column(name = "difficulty")
     @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
+
+    @OneToMany(mappedBy = "question")
+    private List<Favorites> favorites;
 }
