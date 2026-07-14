f4d1155fe4343d68692e78a24baa10b8a23dfe98
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-14 15:58:18 +0900

    refactor: map Favorites entity to User and Question

--- english-output-trainer/src/main/java/com/example/demo/entity/Favorites.java
+++ english-output-trainer/src/main/java/com/example/demo/entity/Favorites.java
@@ -5,16 +5,30 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.Data;
+
 
 @Data
 @Entity
 @Table(name = "favorites")
 public class Favorites {
-	@EmbeddedId
-	private FavoritesKey favoritesKey;
-	
-	private LocalDateTime createdAt;
-	
+
+    @EmbeddedId
+    private FavoritesKey favoritesKey;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @ManyToOne
+    @MapsId("questionId")
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    private LocalDateTime createdAt;
 }
