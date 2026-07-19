cebeeee5e283a31ed3587aaef2aa2cd9262aca25
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-19 22:33:39 +0900

    refactor: enable auto generation of question IDs

--- english-output-trainer/src/main/java/com/example/demo/entity/Question.java
+++ english-output-trainer/src/main/java/com/example/demo/entity/Question.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -18,9 +20,10 @@ import lombok.Setter;
 @Table(name = "question")
 public class Question {
 
-    @Id
-    @Column(name = "question_id")
-    private Long questionId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "question_id")
+	private Long questionId;
 
     @Column(name = "japanese_text")
     private String japaneseText;
