5a966e91d610b5b88135e2f06ec432eae7e149a3
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-19 22:32:41 +0900

    feat: add question creation service

--- english-output-trainer/src/main/java/com/example/demo/service/AdminService.java
+++ english-output-trainer/src/main/java/com/example/demo/service/AdminService.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.entity.Question;
+import com.example.demo.form.QuestionForm;
+import com.example.demo.repository.QuestionRepository;
 import com.example.demo.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -12,10 +15,28 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class AdminService {
-	private final UserRepository repository;
+	private final UserRepository userRepository;
+	private final QuestionRepository questionRepository;
 	
 	@Transactional
 	public void deleteOneUser(String userId) {
-		repository.deleteByUserId(userId);
+		userRepository.deleteByUserId(userId);
 	}
+	
+	public void addQuestion(QuestionForm form) {
+		
+    	Question question = new Question();
+
+    	question.setJapaneseText(form.getJapaneseText());
+    	question.setEnglishText(form.getEnglishText());
+    	question.setAlternativeAnswer(form.getAlternativeAnswer());
+    	question.setCondition(form.getCondition());
+    	question.setDifficulty(form.getDifficulty());
+    	
+    	Question savedQuestion = questionRepository.save(question);
+    	
+    	log.info("問題登録完了 questionId={}", savedQuestion.getQuestionId());
+		
+	}
+		
 }
