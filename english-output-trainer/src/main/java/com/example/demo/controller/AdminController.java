cbd11826c9afacb4d0551d6346c02b0e4ebfc1b2
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-19 22:43:04 +0900

    feat: add controller for question management

--- english-output-trainer/src/main/java/com/example/demo/controller/AdminController.java
+++ english-output-trainer/src/main/java/com/example/demo/controller/AdminController.java
@@ -5,20 +5,25 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Users;
+import com.example.demo.form.QuestionForm;
+import com.example.demo.service.AdminService;
 import com.example.demo.service.UserServiceImpl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
-
+@Slf4j
 public class AdminController {
 	
 	private final UserServiceImpl userServiceImpl;
+	private final AdminService adminService;
 	
 	@GetMapping("/admin")
 	public String getAdmin() {
@@ -44,5 +49,41 @@ public class AdminController {
 							 Model model){
 		userServiceImpl.deleteUserOne(userId);
 		return "redirect:/admin/list";
+	}
+	
+	@GetMapping("/admin/question/list")
+	public String getQuestionList() {
+		return "admin/question/list";
+	}
+	
+	@GetMapping("/admin/question/add")
+	public String getQuestionAdd(
+	        Model model) {
+
+	    model.addAttribute(
+	            "questionForm",
+	            new QuestionForm());
+
+	    return "admin/question/add";
+	}
+	
+	@PostMapping("/admin/question/add")
+	public String postQuestionAdd(
+			//@ModelAttribute @Validated QuestionForm form
+			//BindingResult bindingResult
+			@ModelAttribute QuestionForm form,
+			Model model) {
+		
+//		① 通常のバリデーションエラー確認
+//	    if (bindingResult.hasErrors()) {
+//	        return getQuestionAdd(model, form);
+//	    }
+
+		
+		log.info("問題登録 {}", form);  
+		adminService.addQuestion(form);
+		
+		return "redirect:/admin/question/list";
+
 	}
 }
