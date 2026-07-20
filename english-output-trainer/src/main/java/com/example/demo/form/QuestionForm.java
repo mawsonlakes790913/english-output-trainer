c7cd194a8b6bec91cb64a9dfc20f817d651afbce
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-20 15:32:18 +0900

    feat: add validation to question form

--- english-output-trainer/src/main/java/com/example/demo/form/QuestionForm.java
+++ english-output-trainer/src/main/java/com/example/demo/form/QuestionForm.java
@@ -1,19 +1,37 @@
 package com.example.demo.form;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.example.demo.entity.Difficulty;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class QuestionForm {
-
+	
+	@NotBlank
+	@Length(max = 100)
     private String japaneseText;
 
+	@NotBlank
+	@Length(max = 200)
+    @Pattern(
+            regexp = "^[a-zA-Z0-9 .,!?:;'\"()/-]+$"
+        )
     private String englishText;
-
+	
+	@Length(max = 200)
+    @Pattern(
+            regexp = "^[a-zA-Z0-9 .,!?:;'\"()/-]*$"
+        )
     private String alternativeAnswer;
 
+	@Length(max = 20)
     private String condition;
 
+	@NotNull
     private Difficulty difficulty;
 }
