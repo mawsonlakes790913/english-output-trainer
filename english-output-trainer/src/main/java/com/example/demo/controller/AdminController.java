1985125ff1483b8b10c5f69f6aecdc0093f479ce
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-20 15:53:32 +0900

    feat: enable validation for question registration

--- english-output-trainer/src/main/java/com/example/demo/controller/AdminController.java
+++ english-output-trainer/src/main/java/com/example/demo/controller/AdminController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -69,15 +71,14 @@ public class AdminController {
 	
 	@PostMapping("/admin/question/add")
 	public String postQuestionAdd(
-			//@ModelAttribute @Validated QuestionForm form
-			//BindingResult bindingResult
-			@ModelAttribute QuestionForm form,
+			@ModelAttribute @Validated QuestionForm form,
+			BindingResult bindingResult,
 			Model model) {
 		
-//		① 通常のバリデーションエラー確認
-//	    if (bindingResult.hasErrors()) {
-//	        return getQuestionAdd(model, form);
-//	    }
+		//① 通常のバリデーションエラー確認
+	    if (bindingResult.hasErrors()) {
+	        return "admin/question/add";
+	    }
 
 		
 		log.info("問題登録 {}", form);  
