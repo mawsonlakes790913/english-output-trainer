0173fc902812a996793a29f180a690a534435568
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-21 20:16:54 +0900

    feat: add query to retrieve questions in descending ID order

--- english-output-trainer/src/main/java/com/example/demo/repository/QuestionRepository.java
+++ english-output-trainer/src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -54,6 +56,8 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 				    @Param("userId") Long userId,
 				    @Param("difficulties") List<String> difficulties					
 					);
+	
+	Page<Question> findAllByOrderByQuestionIdDesc(Pageable pageable);
 	
 	
 }
