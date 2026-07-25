ebf995c2a88be0a180ce0d20ac308862782f90b8
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-26 02:37:35 +0900

    refactor: remove converter methods from ReviewService and UserService

--- english-output-trainer/src/main/java/com/example/demo/service/UserService.java
+++ english-output-trainer/src/main/java/com/example/demo/service/UserService.java
@@ -211,21 +211,4 @@ public class UserService {
 	            pageable);
 	}
 	
-//	public String convertStudyCondition(StudyCondition studyCondition) {
-//		
-//		String convertedStudyCondition;
-//		
-//		if (studyCondition == null) {
-//			
-//			convertedStudyCondition = StudyCondition.ALL.name();
-//			
-//		} else {
-//			
-//			convertedStudyCondition = studyCondition.name();
-//					
-//		}
-//		
-//		return convertedStudyCondition;
-//	}
-	
 }
