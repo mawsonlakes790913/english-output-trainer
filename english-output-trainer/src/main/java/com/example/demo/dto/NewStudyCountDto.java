a6d169f03a2665647efc5b1f404ef8cae03ffb87
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-31 03:01:38 +0900

    refactor: simplify DTOs using Lombok

--- english-output-trainer/src/main/java/com/example/demo/dto/NewStudyCountDto.java
+++ english-output-trainer/src/main/java/com/example/demo/dto/NewStudyCountDto.java
@@ -1,28 +1,12 @@
 package com.example.demo.dto;
 
+import lombok.Data;
+
+@Data
 public class NewStudyCountDto {
 
     private long beginnerCount;
     private long intermediateCount;
     private long advancedCount;
-    
-	public long getBeginnerCount() {
-		return beginnerCount;
-	}
-	public void setBeginnerCount(long beginnerCount) {
-		this.beginnerCount = beginnerCount;
-	}
-	public long getIntermediateCount() {
-		return intermediateCount;
-	}
-	public void setIntermediateCount(long intermediateCount) {
-		this.intermediateCount = intermediateCount;
-	}
-	public long getAdvancedCount() {
-		return advancedCount;
-	}
-	public void setAdvancedCount(long advancedCount) {
-		this.advancedCount = advancedCount;
-	}
 
 }
