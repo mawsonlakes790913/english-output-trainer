889023e7ebb156e75f85a5005990b069a280253f
mawsonlakes790913 <liuniao790913@gmail.com>
2026-07-31 03:38:21 +0900

    refactor: remove unused age field from SignupForm

--- english-output-trainer/src/main/java/com/example/demo/form/SignupForm.java
+++ english-output-trainer/src/main/java/com/example/demo/form/SignupForm.java
@@ -4,8 +4,6 @@ import org.hibernate.validator.constraints.Length;
 
 import com.example.demo.validator.PasswordMatch;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -21,10 +19,6 @@ public class SignupForm {
 	@Length(min = 8, max = 20)
 	@Pattern( regexp = "^[a-zA-Z0-9]+$")
 	private String userId;
-	
-	@Min(0)
-	@Max(120)
-	private Integer age;
 	
 	@NotBlank
 	@Length(min = 12, max = 30)
